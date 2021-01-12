b7d8ffcf9efbb99761bb2957ef1d175eaaae1d66
Lucas Maciel Vieira <lucas@Lucass-MacBook-Pro.local>
2021-01-11 22:06:15 -0300

    [Cypress] added simple e2e testing

--- cypress/integration/App.cy-spec.js
+++ cypress/integration/App.cy-spec.js
@@ -7,5 +7,20 @@ context('Simple render and routing', () => {
 
   it('loads normal page and try to access user page without login', () => {
     cy.visit('/')
+
+    // check if header is rendered
+    cy.get('.header-component').should('be.visible')
+
+    // check if footer is rendered
+    cy.get('.footer-component').should('be.visible')
+
+    // expect to have many offers as mock call have many of them
+    cy.waitUntil(() => {
+      return cy.get('.ant-spin')
+        .should('not.exist')
+        .then(() => {
+          cy.get('.offer-card-component').its('length').should('be.gte', 1)
+        })
+    })
   })
 })
